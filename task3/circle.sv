96e7931ba42ce1d5c9149bff3163d147dae9e6fd
peegggy <peggyyuanvan@gmail.com>
2021-06-15 12:40:45 -0700

    code works with comments

--- task3/circle.sv
+++ task3/circle.sv
@@ -1,17 +1,16 @@
 `timescale 1ps/1ps
-`define reset   4'd0;
-`define compare 4'd1
-`define oct1    4'd2;
-`define oct2    4'd3;
-`define oct4    4'd4;
-`define oct3    4'd5;
-`define oct5    4'd6;
-`define oct6    4'd7;
-`define oct8    4'd8;
-`define oct7    4'd9;
-`define Yincre  4'd10;
-`define crit    4'd11;
-`define done    4'd12;
+`define reset   4'd0;//set offet_y = 0, offset_x to radius and crit to 1-radius
+`define compare 4'd1;//check if offset_y is <= offset_x
+`define oct1    4'd2;//draw octant1
+`define oct2    4'd3;//draw octant2
+`define oct4    4'd4;//draw octant4
+`define oct3    4'd5;//draw octant3
+`define oct5    4'd6;//draw octant5
+`define oct6    4'd7;//draw octant6
+`define oct8    4'd8;//draw octant8
+`define oct7    4'd9;//draw octant7
+`define crit    4'd10;//check crit condition
+`define done    4'd11;//circle done
 
 /*drawCircle(centre_x, centre_y, radius):
     offset_y = 0
@@ -33,6 +32,8 @@
             offset_x = offset_x - 1
             crit = crit + 2 * (offset_y - offset_x) + 1
 */
+//ths purpose of this module is to implement the above psuedo code using systemverilog to draw a circle
+//of specified centre and radius. It should not draw anything when the pixels are out of bound
 module circle(input logic clk, input logic rst_n, input logic [2:0] colour,
               input logic [7:0] centre_x, input logic [6:0] centre_y, input logic [7:0] radius,
               input logic start, output logic done,
@@ -40,9 +41,13 @@ module circle(input logic clk, input logic rst_n, input logic [2:0] colour,
               output logic [2:0] vga_colour, output logic vga_plot);
      // draw the circle
      logic [3:0] state, nextstate;
-     logic signed [7:0] offset_x, offset_y, crit;
+     //indicate how far off the coord is from the centre
+     logic [7:0] offset_x, offset_y;
+     logic signed [8:0] crit;
+     //if offset_x < offset_y, then finish the statemachine
      logic xLessThan;
 
+     //this always block indicates what the next state is
      always_comb begin
           case(state)
           //reset -> compare if start is asserted
@@ -63,39 +68,41 @@ module circle(input logic clk, input logic rst_n, input logic [2:0] colour,
           4'd7 : nextstate = 4'd8;
           //oct8 -> oct7
           4'd8 : nextstate = 4'd9;
-          //oct7 -> Yincre
+          //oct7 -> crit
           4'd9 : nextstate = 4'd10;
-          //Yincre -> crit
-          4'd10 : nextstate = 4'd11;
           //crit -> compare
-          4'd11 : nextstate = 4'd1;
-          //done -> done
-          4'd12 : nextstate = 4'd12;
-          default : nextstate = 4'd12;
+          4'd10 : nextstate = 4'd1;
+          //done -> reset if start is deasserted
+          4'd11 : nextstate = ~start ? 4'd0 : 4'd11;
+          default : nextstate = 4'd11;
           endcase
      end
-
+     //this always block indicates what the current state is
      always_ff @(posedge clk) begin
-          if(~rst_n)
-          state <= 4'd0;
+          if(~rst_n)//if reset is asserted
+          state <= 4'd0;//then go to reset state
           else
-          state <= nextstate;
+          state <= nextstate;//else nextstate becomes the current state
      end
-
+     //this always block updates the output wires or any internal wires on the posedge 
+     //of the clock in the current state
      always_ff @(posedge clk) begin
           case(state)
           4'd0 : begin //reset
-               offset_y <= 8'b0;
-               offset_x <= radius;
-               crit <= $signed(8'd1) - $signed(radius);
+               offset_y <= 8'b0;//according to the pseudo code, offset_y is 0
+               offset_x <= radius;//and offset_x is radius
+               //crit is 1-radius which is in the negative range, thats why there is $signed()
+               crit <= $signed(9'd1) - $signed(radius);
                done <= 1'b0;
+               //---set the vga wires to 0--
                vga_x <= 8'b0;
                vga_y <= 7'b0;
-               vga_colour <= 3'b010;//colour is green
+               vga_colour <= 3'b000;
                vga_plot <= 1'b0;
                xLessThan <= 0;
           end
           4'd1 : begin //compare
+               vga_colour <= colour; //colour is green
                if(offset_y <= offset_x)
                xLessThan <= 0;
                else
@@ -107,49 +114,85 @@ module circle(input logic clk, input logic rst_n, input logic [2:0] colour,
           4'd2 : begin //oct1
                vga_x <= (centre_x + offset_x);
                vga_y <= (centre_y + offset_y);
-               vga_plot <= 1;
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x+offset_x)&&(centre_x+offset_x)<=$signed(8'd159) && $signed(7'd0)<=(centre_y+offset_y)&&(centre_y+offset_y)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd3 : begin //oct2
                vga_x <= (centre_x + offset_y);
                vga_y <= (centre_y + offset_x); 
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x+offset_y)&&(centre_x+offset_y)<=$signed(8'd159) && $signed(7'd0)<=(centre_y+offset_x)&&(centre_y+offset_x)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd4 : begin //oct4
                vga_x <= (centre_x - offset_x);
                vga_y <= (centre_y + offset_y);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x-offset_x)&&(centre_x-offset_x)<=$signed(8'd159) && $signed(7'd0)<=(centre_y+offset_y)&&(centre_y+offset_y)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd5 : begin //oct3
                vga_x <= (centre_x - offset_y);
                vga_y <= (centre_y + offset_x);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x-offset_y)&&(centre_x-offset_y)<=$signed(8'd159) && $signed(7'd0)<=(centre_y+offset_x)&&(centre_y+offset_x)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd6 : begin //oct5
                vga_x <= (centre_x - offset_x);
                vga_y <= (centre_y - offset_y);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x-offset_x)&&(centre_x-offset_x)<=$signed(8'd159) && $signed(7'd0)<=(centre_y-offset_y)&&(centre_y-offset_y)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd7 : begin //oct6
                vga_x <= (centre_x - offset_y);
                vga_y <= (centre_y - offset_x);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x-offset_y)&&(centre_x-offset_y)<=$signed(8'd159) && $signed(7'd0)<=(centre_y-offset_x)&&(centre_y-offset_x)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd8 : begin //oct8
                vga_x <= (centre_x + offset_x);
                vga_y <= (centre_y - offset_y);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x+offset_x)&&(centre_x+offset_x)<=$signed(8'd159) && $signed(7'd0)<=(centre_y-offset_y)&&(centre_y-offset_y)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
           4'd9 : begin //oct7
                vga_x <= (centre_x + offset_y);
                vga_y <= (centre_y - offset_x);
+               //--checking for boundary conditions, we know the limit for x and y coords so use that as the boundary---
+               if($signed(8'd0)<=(centre_x+offset_y)&&(centre_x+offset_y)<=$signed(8'd159) && $signed(7'd0)<=(centre_y-offset_x)&&(centre_y-offset_x)<=$signed(7'd119))
+               vga_plot <= 1;//if the coord is within the bounds, then plot
+               else vga_plot <= 0;
           end
-          4'd10 : begin //Yincre
-               offset_y <= offset_y + 8'd1;
-          end
-          4'd11 : begin //crit
-               if(crit <= $signed(8'd0))
-               crit <= crit + (8'd2*offset_y) + 8'd1;
+          //---- finish the 8 octants -----
+          4'd10 : begin //crit
+          //increment offset_y first
+          offset_y <= offset_y + 8'd1;
+          //and not plotting anything
+          vga_plot <= 0;
+               //check if crit is negative
+               if(crit <= $signed(9'd0))
+               //reassign crit
+               crit <= crit + (9'd2*offset_y) + 9'd1;
                else begin
+                    //else decrement offset_x
                     offset_x <= offset_x - 8'd1;
-                    crit <= crit + (8'd2*(offset_y - offset_x)) + 8'd1;
+                    crit <= crit + (9'd2*(offset_y - offset_x)) + 9'd1;
                end
           end
-          4'd12 : begin //done
-               done <= 1'b1;
+          4'd11 : begin //done
+               done <= 1'b1;//assert done when the circle is done
                vga_plot <= 1'b0;
                xLessThan <= 0;
           end
--- task3/task3.sv
+++ task3/task3.sv
@@ -1,4 +1,10 @@
 `timescale 1 ps / 1 ps
+`define fillscreen 2'd0; //start the fillscreen module
+`define circle     2'd1; //start the circle module
+`define done       2'd2;
+
+//The purpose of this top module is to implement the fillscreen that fills the whole screen
+//black before drawing the circle
 module task3(input logic CLOCK_50, input logic [3:0] KEY,
              input logic [9:0] SW, output logic [9:0] LEDR,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
@@ -21,29 +27,92 @@ assign VGA_B = VGA_B_10[9:2];
                                             .x(VGA_X), .y(VGA_Y), .plot(VGA_PLOT),
                                             .VGA_R(VGA_R_10), .VGA_G(VGA_G_10), .VGA_B(VGA_B_10),
                                             .*);
-    logic start, done;
-    logic [2:0] colour;
+    //seperate start and done signals for the 2 modules
+    logic start_screen, start_circle, done_screen, done_circle;
+    //stats and the next states
+    logic [1:0] state, nextstate;
+    //seperate x coordinates for fillscreen and circle
+    logic [7:0] vga_x_fillscreen, vga_x_circle;
+    //seperate y coordinates for fillscreen and circle
+    logic [6:0] vga_y_fillscreen, vga_y_circle;
+    //seperate colour wires for fillscreen and circle
+    logic [2:0] vga_colour_fillscreen, vga_colour_circle;
+    //seperate plot flags for fillscreen and circle
+    logic vga_plot_fillscreen, vga_plot_circle;
 
+    //instantiates the fillscreen module to fill the screen black first
+    fillscreen f (.clk(CLOCK_50),
+                  .rst_n(KEY[3]),
+                  .colour(3'b0), //3'b000 is black
+                  .start(start_screen),
+                  .done(done_screen),
+                  .vga_x(vga_x_fillscreen),
+                  .vga_y(vga_y_fillscreen),
+                  .vga_colour(vga_colour_fillscreen),
+                  .vga_plot(vga_plot_fillscreen));
 
-    always_comb begin
-        if(KEY[3] == 1'b0)//if reset is pressed
-        start = 1; //start is asserted
-        else if(done) //if done is asserted
-        start = 0; //start is deasserted
-        else start = 1; //else start remains 1
-    end
-
+    //instantiates the circle module to draw a circle
     circle c (.clk(CLOCK_50),
               .rst_n(KEY[3]),
-              .colour(colour),
+              .colour(3'b010),//3'b010 is green
               .centre_x(8'd80),
               .centre_y(8'd60),
               .radius(8'd40),
-              .start(start),
-              .done(done),
-              .vga_x(VGA_X),
-              .vga_y(VGA_Y),
-              .vga_colour(VGA_COLOUR),
-              .vga_plot(VGA_PLOT));
+              .start(start_circle),
+              .done(done_circle),
+              .vga_x(vga_x_circle),
+              .vga_y(vga_y_circle),
+              .vga_colour(vga_colour_circle),
+              .vga_plot(vga_plot_circle));
 
+    //this always block indicates what the current state is
+    always_ff @(posedge CLOCK_50) begin
+          if(~KEY[3])//if reset is asserted
+          state <= 2'd0; //go back to fillscreen
+          else
+          state <= nextstate; //nextstate becomes the current state
+     end
+    //this always block indicates what the next state is
+     always_comb begin
+         case(state)
+         //when done_screen is asserted go to draw circle state, if not, then stay in fillscreen
+         2'd0 : nextstate = done_screen ? 2'd1 : 2'd0;
+         //when draw circle is done then go to done state
+         2'd1 : nextstate = done_circle ? 2'd2 : 2'd1;
+         default: nextstate = 2'd2;
+         endcase
+     end
+     //this always block updates the output signals or any internal signals on the posedge 
+     //of the clock in the current state
+     always_ff @(posedge CLOCK_50) begin
+         case(state)
+         2'd0 : begin
+             start_screen <= 1; //to start the fillscreen module
+             start_circle <= 0; //but do not start the circle module
+             //the vga signals from fillscreen are assigned to the VGA adaptor
+             VGA_X <= vga_x_fillscreen;
+             VGA_Y <= vga_y_fillscreen;
+             VGA_COLOUR <= vga_colour_fillscreen;
+             VGA_PLOT <= vga_plot_fillscreen;
+         end
+         2'd1 : begin
+             start_circle <= 1; //start the circle module
+             start_screen <= 0; //but do not start the fillscreen module
+             //the vga signals from circle are assigned to the VGA adaptor
+             VGA_X <= vga_x_circle;
+             VGA_Y <= vga_y_circle;
+             VGA_COLOUR <= vga_colour_circle;
+             VGA_PLOT <= vga_plot_circle;
+         end
+         2'd2 : begin
+             //done state sets every signals back to 0
+             start_circle <= 0;
+             start_screen <= 0;
+             VGA_COLOUR <= 0;
+             VGA_PLOT <= 0;
+             VGA_X <= 0;
+             VGA_Y <= 0;
+         end
+     endcase
+     end
 endmodule: task3
